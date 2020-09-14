2e31a5aa80a1febbfac89e71bacdf07f3d8cbb50
tara <t.zohdinasab@gmail.com>
2020-09-14 05:35:04 +0430

    properties.py modified

--- DeepJanus-MNIST/properties.py
+++ DeepJanus-MNIST/properties.py
@@ -2,51 +2,51 @@
 import os
 
 # GA Setup
-POPSIZE          = int(os.getenv('DH_POPSIZE', '800'))
-NGEN             = int(os.getenv('DH_NGEN', '500000'))
+POPSIZE          = int(os.getenv('DJ_POPSIZE', '800'))
+NGEN             = int(os.getenv('DJ_NGEN', '500000'))
 
-RUNTIME          = int(os.getenv('DH_RUNTIME', '3600'))
-INTERVAL         = int(os.getenv('DH_INTERVAL', '900'))
+RUNTIME          = int(os.getenv('DJ_RUNTIME', '3600'))
+INTERVAL         = int(os.getenv('DJ_INTERVAL', '900'))
 
 # Mutation Hyperparameters
 # range of the mutation
-MUTLOWERBOUND    = float(os.getenv('DH_MUTLOWERBOUND', '0.01'))
-MUTUPPERBOUND    = float(os.getenv('DH_MUTUPPERBOUND', '0.6'))
+MUTLOWERBOUND    = float(os.getenv('DJ_MUTLOWERBOUND', '0.01'))
+MUTUPPERBOUND    = float(os.getenv('DJ_MUTUPPERBOUND', '0.6'))
 
 # Dataset
-EXPECTED_LABEL   = int(os.getenv('DH_EXPECTED_LABEL', '5'))
+EXPECTED_LABEL   = int(os.getenv('DJ_EXPECTED_LABEL', '5'))
 
 #------- NOT TUNING ----------
 
 # mutation operator probability
-MUTPB            = float(os.getenv('DH_MUTPB', '0.7'))
-MUTOPPROB        = float(os.getenv('DH_MUTOPPROB', '0.5'))
-MUTOFPROB        = float(os.getenv('DH_MUTOFPROB', '0.5'))
+MUTPB            = float(os.getenv('DJ_MUTPB', '0.7'))
+MUTOPPROB        = float(os.getenv('DJ_MUTOPPROB', '0.5'))
+MUTOFPROB        = float(os.getenv('DJ_MUTOFPROB', '0.5'))
 
 
-IMG_SIZE         = int(os.getenv('DH_IMG_SIZE', '28'))
-num_classes      = int(os.getenv('DH_NUM_CLASSES', '10'))
+IMG_SIZE         = int(os.getenv('DJ_IMG_SIZE', '28'))
+num_classes      = int(os.getenv('DJ_NUM_CLASSES', '10'))
 
-INITIALPOP       = os.getenv('DH_INITIALPOP', 'seeded')
+INITIALPOP       = os.getenv('DJ_INITIALPOP', 'seeded')
 
-MODEL            = os.getenv('DH_MODEL', 'models/model_mnist.h5')
+MODEL            = os.getenv('DJ_MODEL', 'models/model_mnist.h5')
 
-ORIGINAL_SEEDS   = os.getenv('DH_ORIGINAL_SEEDS', 'bootstraps_five')
+ORIGINAL_SEEDS   = os.getenv('DJ_ORIGINAL_SEEDS', 'bootstraps_five')
 
-BITMAP_THRESHOLD = float(os.getenv('DH_BITMAP_THRESHOLD', '0.5'))
+BITMAP_THRESHOLD = float(os.getenv('DJ_BITMAP_THRESHOLD', '0.5'))
 
-DISTANCE         = float(os.getenv('DH_DISTANCE', '2.0'))
+DISTANCE         = float(os.getenv('DJ_DISTANCE', '2.0'))
 
 
 # Reseeding Hyperparameters
 # extent of the reseeding operator
-RESEEDUPPERBOUND = int(os.getenv('DH_RESEEDUPPERBOUND', '10'))
+RESEEDUPPERBOUND = int(os.getenv('DJ_RESEEDUPPERBOUND', '10'))
 
 
-K_SD             = float(os.getenv('DH_K_SD', '0.1'))
+K_SD             = float(os.getenv('DJ_K_SD', '0.1'))
 
 # K-nearest
-K                = int(os.getenv('DH_K', '1'))
+K                = int(os.getenv('DJ_K', '1'))
 
 # Archive configuration
 ARCHIVE_THRESHOLD = float(os.getenv('ARCHIVE_THRESHOLD', '2.0'))
