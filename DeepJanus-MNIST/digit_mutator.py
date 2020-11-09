4096620f6ae821445d8db1ed8f3bfcd2ae378970
Vincenzo Riccio <vincenzo.riccio@unina.it>
2020-11-09 22:45:51 +0100

    fix mutator

--- DeepJanus-MNIST/digit_mutator.py
+++ DeepJanus-MNIST/digit_mutator.py
@@ -44,7 +44,7 @@ class DigitMutator:
         #TODO: trial
         self.digit.predicted_label = None
         self.digit.confidence = None
-        self.correctly_predicted = None
+        self.digit.correctly_classified = None
 
         return distance_inputs
 
